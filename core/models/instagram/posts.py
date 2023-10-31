a94aa6d668217d2e516230a4d7c1347f53c42805
= <irismickens@gmail.com>
2023-10-31 17:02:42 -0500

    Update posts.py

--- core/models/instagram/posts.py
+++ core/models/instagram/posts.py
@@ -1,4 +1,6 @@
-from httpx import AsyncClient
+from typing import Dict
+
+import httpx
 from pydantic import BaseModel
 
 from core.config import Api
@@ -8,11 +10,11 @@ class InstagramModel(BaseModel):
     url: str = Api.url
     headers: dict = Api.headers
 
-    async def get_user_story(self: "InstagramModel", username: str) -> dict:
+    async def get_user_story(self, username: str) -> Dict:
         """
-        Get's an Instagram user's story
+        Get an Instagram user's story.
         """
-        async with AsyncClient() as client:
+        async with httpx.AsyncClient() as client:
             response = await client.get(
                 url=f"{self.url}/ig/user/{username}/stories",
                 headers=self.headers,
