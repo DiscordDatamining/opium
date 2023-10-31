48c9eecf275f1ec5da333b1222b63d2d2bbd30a4
= <irismickens@gmail.com>
2023-10-31 16:40:56 -0500

    Update posts.py

--- core/models/instagram/posts.py
+++ core/models/instagram/posts.py
@@ -1,4 +1,5 @@
 from aiohttp import ClientSession
+from httpx import get, post
 from pydantic import BaseModel
 
 from core.config import Api
@@ -8,3 +9,14 @@ class InstagramModel(BaseModel):
     url: str = Api.url
     headers: dict = Api.headers
     session = ClientSession()
+
+    @property
+    def get_user_story(self: "InstagramModel", username: str) -> None:
+        """
+        Get's a instagram user's story
+        """
+        r = get(
+            headers=self.headers,
+            url=f"{self.url}/ig/user/%s/stories" % username,
+        )
+        return r.json()
